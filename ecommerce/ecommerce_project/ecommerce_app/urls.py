30043d8999fd1ab76323a12c4ad4b8cef828cee1
geethugopikrishnan <geethugopikrishnan@gmail.com>
2023-10-06 14:54:08 +0530

    done

---
+++ ecommerce/ecommerce_project/ecommerce_app/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path, include
+app_name= 'ecommerce_app'
+
+urlpatterns = [
+    path('',views.allProdCat,name='allProdCat'),
+    path('<slug:c_slug>/',views.allProdCat,name='product_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='prodCatDetail'),
+
+]
